27dfaed26fa3a46fc89edf71b0094bb2ceeb42e1
imasson <imasson@summasolutions.net>
2015-12-02 10:37:33 -0300

    WIP

--- v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Model/Standard/Payment.php
+++ v1.6.x - 1.9.x - Transparent/app/code/community/MercadoPago/Core/Model/Standard/Payment.php
@@ -215,7 +215,8 @@ class MercadoPago_Core_Model_Standard_Payment
             "failure" => Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK, true) . "mercadopago/success"
         ];
 
-        $arr['notification_url'] = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK, true) . "mercadopago/notifications?checkout=standard";
+        //$arr['notification_url'] = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK, true) . "mercadopago/notifications?checkout=standard";
+        $arr['notification_url'] = "http://ba65e9fe.ngrok.io/mercadopago/notifications/standard";
 
         $arr['payment_methods']['excluded_payment_methods'] = $this->getExcludedPaymentsMethods();
         $installments = $this->getConfigData('installments');
