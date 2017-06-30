dac8439729de01dc871fd2c3cd7d7442748e207e
imasson <imasson@summasolutions.net>
2017-06-30 15:13:06 -0300

    Added px-php

---
+++ 1.6.x-1.9.x/lib/MercadoPago/Lib/RestClient.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * MercadoPago cURL RestClient
+ */
+
+
+class MercadoPago_Lib_RestClient {
+
+    const API_BASE_URL = "https://api.mercadopago.com";
+
+    private static function get_connect($uri, $method, $content_type, $extra_params = array()) {
+        if (!extension_loaded ("curl")) {
+            throw new Exception("cURL extension not found. You need to enable cURL in your php.ini or another configuration you have.");
+        }
+
+        $connect = curl_init(self::API_BASE_URL . $uri);
+
+        curl_setopt($connect, CURLOPT_USERAGENT, "MercadoPago Magento-1.9.x-transparent Cart v1.0.2");
+        curl_setopt($connect, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($connect, CURLOPT_CUSTOMREQUEST, $method);
+
+        $header_opt = array("Accept: application/json", "Content-Type: " . $content_type);
+        if (count($extra_params) > 0) {
+            $header_opt = array_merge($header_opt, $extra_params);
+        }
+
+        curl_setopt($connect, CURLOPT_HTTPHEADER, $header_opt);
+
+        return $connect;
+    }
+
+    private static function set_data(&$connect, $data, $content_type) {
+        if ($content_type == "application/json") {
+            if (gettype($data) == "string") {
+                json_decode($data, true);
+            } else {
+                $data = json_encode($data);
+            }
+
+            if(function_exists('json_last_error')) {
+                $json_error = json_last_error();
+                if ($json_error != JSON_ERROR_NONE) {
+                    throw new Exception("JSON Error [{$json_error}] - Data: {$data}");
+                }
+            }
+        }
+
+        curl_setopt($connect, CURLOPT_POSTFIELDS, $data);
+    }
+
+    private static function exec($method, $uri, $data, $content_type, $extra_params) {
+        $connect = self::get_connect($uri, $method, $content_type, $extra_params);
+        if ($data) {
+            self::set_data($connect, $data, $content_type);
+        }
+
+        $api_result = curl_exec($connect);
+        $api_http_code = curl_getinfo($connect, CURLINFO_HTTP_CODE);
+
+        if ($api_result === FALSE) {
+            throw new Exception (curl_error ($connect));
+        }
+
+        $response = array(
+            "status" => $api_http_code,
+            "response" => json_decode($api_result, true)
+        );
+
+        /*if ($response['status'] >= 400) {
+            $message = $response['response']['message'];
+            if (isset ($response['response']['cause'])) {
+                if (isset ($response['response']['cause']['code']) && isset ($response['response']['cause']['description'])) {
+                    $message .= " - ".$response['response']['cause']['code'].': '.$response['response']['cause']['description'];
+                } else if (is_array ($response['response']['cause'])) {
+                    foreach ($response['response']['cause'] as $cause) {
+                        $message .= " - ".$cause['code'].': '.$cause['description'];
+                    }
+                }
+            }
+
+            throw new Exception ($message, $response['status']);
+        }*/
+
+        curl_close($connect);
+
+        return $response;
+    }
+
+    public static function get($request) {
+        //$request['url_query'] = $request['params'];
+        \MercadoPago\MercadoPagoSdk::initialize();
+        return \MercadoPago\MercadoPagoSdk::restClient()->get($request['uri'], $request);
+    }
+
+    public static function post($request) {
+        $request['url_query'] = $request['params'];
+        $request['json_data'] = $request['data'];
+
+        return \MercadoPago\MercadoPagoSdk::restClient()->post($request['uri'], $request);
+    }
+
+    public static function put($request) {
+        $request['url_query'] = $request['params'];
+        $request['json_data'] = $request['data'];
+
+        return \MercadoPago\MercadoPagoSdk::restClient()->put($request['uri'], $request);
+    }
+
+    public static function delete($request) {
+        $request['url_query'] = $request['params'];
+        $request['json_data'] = $request['data'];
+
+        return \MercadoPago\MercadoPagoSdk::restClient()->delete($request['uri'], $request);
+    }
+}
